5a72ed237b9dacfb4e09ea43cd2ddc8bca33ca82
Franklin-tech01 <franklinmike2021@gmail.com>
2025-05-04 00:12:58 +0100

    add OurVision and AccountSetup components to the homepage; update Hero section layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/home/hero";
 import TargetAudience from "@/components/home/target-audience";
 import KeyFeatures from "@/components/home/key-features";
 import Navbar from "@/components/layout/navigation";
+import OurVision from "@/components/home/vision";
+import AccountSetup from "@/components/home/account-setup";
 import React from "react";
 
 const Home = () => {
@@ -13,6 +15,21 @@ const Home = () => {
 			<TargetAudience />
 			{/* Key Features Section */}
 			<KeyFeatures />
+			{/* Our Vision Section */}
+			<OurVision />
+			{/* Account Setup Section */}
+			<AccountSetup />
+			{/* Call to Action Section */}
+			{/* <CallToAction /> */}
+			{/* Testimonials Section */}
+			{/* <Testimonials /> */}
+			{/* Blog Section */}
+			{/* <Blog /> */}
+			{/* FAQ Section */}
+			{/* <FAQ /> */}
+			{/* Contact Us Section */}
+			{/* <ContactUs /> */}
+			{/* Footer Section */}
 		</React.Fragment>
 	);
 };
